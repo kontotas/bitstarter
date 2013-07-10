b7d75384f5c1ed70d7c9d5ee60ea607cf1c5b17c
Ioanna Kontotasiou <ubuntu@ip-172-31-29-39.us-west-2.compute.internal>
2013-07-10 19:30:24 +0000

    Fix web.js

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
-var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var index_buffer = new Buffer(fs.readFile('index.html'));
-
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send(index_buffer.toString());
+ response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+ console.log("Listening on " + port);
 });
+
