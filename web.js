f054970490ab170e4b58075870bd254a227a06ae
Ioanna Kontotasiou <ubuntu@ip-172-31-29-39.us-west-2.compute.internal>
2013-07-10 19:03:33 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var index_buffer = new Buffer(fs.readFile('index.html'));
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index_buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
